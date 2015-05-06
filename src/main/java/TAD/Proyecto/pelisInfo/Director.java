3bd57285f154af8296ccec33cf8ceda731218bed
Patricia <Patricia@patry-jh>
2015-05-06 09:15:35 +0200

    director

---
+++ src/main/java/TAD/Proyecto/pelisInfo/Director.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TAD.Proyecto.pelisInfo;
+
+/**
+ *
+ * @author Patricia
+ */
+public class Director {
+    private int idDirector;
+    private String nombre;
+    private String apellidos;
+
+    public Director(int idDirector, String nombre, String apellidos) {
+        this.idDirector = idDirector;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+    public int getIdDirector() {
+        return idDirector;
+    }
+
+    public void setIdDirector(int idDirector) {
+        this.idDirector = idDirector;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " " + apellidos;
+    }
+}
